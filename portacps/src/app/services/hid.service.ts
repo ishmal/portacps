8f5354a15ad37439a6c8369a7d477d950c4166c3
ishmal <ishmal@github.com>
2020-02-13 15:25:01 -0600

    more hid work

--- portacps/src/app/services/hid.service.ts
+++ portacps/src/app/services/hid.service.ts
@@ -2,6 +2,22 @@ import { Injectable } from '@angular/core';
 
 const HID = (<any>window).require('node-hid');
 
+const HID_VendorId = 5538;  // 0x15a2
+const HID_ProductId = 115;  // 0x73
+
+
+const CMD = {
+	WRITE: 87, // W
+	READ: 82, // R
+	CMD: 67, // C
+	BASE: 66, // B
+	ENDR: [ 69, 78, 68, 82 ], // E N D R
+	ENDW: [ 69, 78, 68, 87 ], // E N D W
+	ACK: [ 65 ], // A
+	PRG: [ 2, 80, 82, 79, 71, 82, 65 ], // 2, P R O G R A
+	PRG2: [ 77, 2 ]  // M 2
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +27,7 @@ export class HidService {
 
   download() {
 	const devices = HID.devices();
-	const dev = devices.find(d => d.vendorId === 5538 && d.productId === 115);
+	const dev = devices.find(d => d.vendorId === HID_VendorId && d.productId === HID_ProductId);
 	if (!dev) {
 		console.log("device not found");
 		return;
